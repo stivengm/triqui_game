96c1c3d4fbedb613684f2c0191d2cd0b9b383c74
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-14 17:57:30 -0500

    FIX - Ajuste de splashScreen.

--- lib/ui/screens/splash_screen/splash_screen.dart
+++ lib/ui/screens/splash_screen/splash_screen.dart
@@ -1,8 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:triqui_game/ui/app_style.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, 'playersScreen');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
