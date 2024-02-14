0ba8c16bee8d7930071a2ca4ac69d7528aff3413
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-14 17:53:16 -0500

    FIX - Ajuste de splashScreen.

--- lib/ui/screens/splash_screen/splash_screen.dart
+++ lib/ui/screens/splash_screen/splash_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:triqui_game/ui/app_style.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppConstants.bgColor,
+      body: Center(
+        child: Text("Triqui", style: AppConstants.customFontWhite.copyWith(fontSize: media.width / 4)),
+      ),
+    );
   }
 }
